466fdd7a4ea3863aadf69d00a0e2bddb025dd003
LavanyaVaidya <labulavanya@gmail.com>
2026-07-10 00:59:49 +0530

    Added config file

---
+++ config.py
@@ -0,0 +1,15 @@
+HOME_URL = "https://normlex.ilo.org/dyn/nrmlx_en/f?p=NORMLEXPUB:1:0::NO:::"
+
+FUNDAMENTAL_CODES = {
+    "C029",
+    "P029",
+    "C087",
+    "C098",
+    "C100",
+    "C105",
+    "C111",
+    "C138",
+    "C182",
+    "C155",
+    "C187",
+}
